645d0dbf2d5cc145bc7c986474b6d2fc1a390d8f
Pedro Jesus Quinonez <pjq899@gmail.com>
2024-08-29 14:13:37 -0700

    finished setting up files/folders

---
+++ src/models/pyfile2.py
@@ -0,0 +1,13 @@
+"""Here is some example code"""
+
+def func1():
+	"""
+    Converts a Gin.config_str() to a dict for logging with comet.ml
+    
+    Args:
+        out_dir (str): if not None the location to save the data at.
+    
+    Returns: 
+        (dict): The Gin config as a dict
+    """
+	print("Hello World)
